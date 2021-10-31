4e2a2f49a2f13facc8adc54b44911d8c81933076
NatrixAeria <upezu@student.kit.edu>
2021-10-31 22:03:44 +0100

    Implement 2 instructions and fix one
    - ORA(0f)
    - ORA(12)
    
    - Fix loading in CMP(d7)

--- rsnes/src/instr.rs
+++ rsnes/src/instr.rs
@@ -7,8 +7,8 @@ use crate::timing::Cycles;
 #[rustfmt::skip]
 static CYCLES: [Cycles; 256] = [
     /* ^0 ^1 ^2 ^3 ^4 ^5 ^6 ^7 | ^8 ^9 ^a ^b ^c ^d ^e ^f */
-       7, 0, 7, 0, 5, 3, 5, 6,   3, 2, 2, 4, 0, 4, 6, 0,  // 0^
-       2, 0, 0, 0, 5, 0, 0, 0,   2, 4, 2, 2, 0, 4, 7, 5,  // 1^
+       7, 0, 7, 0, 5, 3, 5, 6,   3, 2, 2, 4, 0, 4, 6, 5,  // 0^
+       2, 0, 5, 0, 5, 0, 0, 0,   2, 4, 2, 2, 0, 4, 7, 5,  // 1^
        6, 0, 8, 0, 3, 3, 5, 0,   4, 2, 2, 5, 4, 4, 0, 0,  // 2^
        2, 0, 0, 0, 0, 0, 0, 0,   2, 4, 2, 0, 0, 4, 0, 5,  // 3^
        6, 0, 0, 0, 1, 3, 5, 0,   3, 2, 2, 3, 3, 4, 0, 0,  // 4^
@@ -354,9 +354,35 @@ impl<B: crate::backend::AudioBackend, FB: crate::backend::FrameBuffer> Device<B,
                     cycles += 2
                 }
             }
+            0x0f => {
+                // ORA - Or A with Absolute Long
+                let addr = self.load();
+                if self.cpu.is_reg8() {
+                    let val = self.read::<u8>(addr) | self.cpu.regs.a8();
+                    self.cpu.regs.set_a8(val);
+                    self.cpu.update_nz8(val);
+                } else {
+                    self.cpu.regs.a |= self.read::<u16>(addr);
+                    self.cpu.update_nz16(self.cpu.regs.a);
+                    cycles += 1
+                }
+            }
             0x10 => {
                 // BPL - Branch if Plus
                 self.branch_near(!self.cpu.regs.status.has(Status::NEGATIVE), &mut cycles)
+            }
+            0x12 => {
+                // ORA - Or A with DP Indirect
+                let addr = self.load_dp_indirect(&mut cycles);
+                if self.cpu.is_reg8() {
+                    let val = self.read::<u8>(addr) | self.cpu.regs.a8();
+                    self.cpu.regs.set_a8(val);
+                    self.cpu.update_nz8(val);
+                } else {
+                    self.cpu.regs.a |= self.read::<u16>(addr);
+                    self.cpu.update_nz16(self.cpu.regs.a);
+                    cycles += 1
+                }
             }
             0x14 => {
                 // TRB - Test and Reset Bits from Direct Page in A
@@ -1740,7 +1766,7 @@ impl<B: crate::backend::AudioBackend, FB: crate::backend::FrameBuffer> Device<B,
             0xd7 => {
                 // CMP - Compare A with DP Indirect Long Indexed, Y
                 // this will also work with decimal mode (TODO: check this fact)
-                let addr = self.load_indirect_indexed_y::<false>(&mut cycles);
+                let addr = self.load_indirect_long_indexed_y(&mut cycles);
                 if self.cpu.is_reg8() {
                     let val = self.read::<u8>(addr);
                     self.compare8(self.cpu.regs.a8(), val);
