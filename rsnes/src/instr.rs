eb4fb8f97c4f58769bf36af1ec3f2dc4346a5d05
NatrixAeria <upezu@student.kit.edu>
2021-11-02 01:26:32 +0100

    Implement 65816's JMP(6c) instruction

--- rsnes/src/instr.rs
+++ rsnes/src/instr.rs
@@ -13,7 +13,7 @@ static CYCLES: [Cycles; 256] = [
        2, 0, 0, 0, 0, 0, 0, 0,   2, 4, 2, 2, 0, 4, 0, 5,  // 3^
        6, 0, 0, 0, 1, 3, 5, 0,   3, 2, 2, 3, 3, 4, 0, 0,  // 4^
        2, 0, 0, 0, 1, 4, 0, 0,   2, 4, 3, 2, 4, 4, 7, 0,  // 5^
-       6, 0, 6, 0, 3, 3, 0, 0,   4, 2, 2, 6, 0, 4, 0, 0,  // 6^
+       6, 0, 6, 0, 3, 3, 0, 0,   4, 2, 2, 6, 5, 4, 0, 0,  // 6^
        2, 0, 0, 0, 4, 4, 0, 0,   2, 4, 4, 4, 6, 4, 7, 5,  // 7^
        2, 6, 4, 0, 3, 3, 3, 6,   2, 0, 2, 3, 4, 4, 4, 5,  // 8^
        2, 0, 0, 0, 4, 4, 0, 6,   2, 5, 2, 2, 4, 5, 5, 5,  // 9^
@@ -927,6 +927,12 @@ impl<B: crate::backend::AudioBackend, FB: crate::backend::FrameBuffer> Device<B,
                 // RTL - Return from subroutine long
                 self.cpu.regs.pc = self.pull();
                 self.cpu.regs.pc.addr = self.cpu.regs.pc.addr.wrapping_add(1);
+            }
+            0x6c => {
+                // JMP - Jump Absolute Indirect
+                let addr = self.load::<u16>();
+                let addr = self.read(Addr24::new(0, addr));
+                self.cpu.regs.pc.addr = addr;
             }
             0x6d => {
                 // ADC - Add absolute with Carry
