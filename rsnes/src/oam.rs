77cb6fcc78a9287d5537af96b07d4b61c9217884
NatrixAeria <upezu@student.kit.edu>
2021-12-04 18:39:06 +0100

    Reimplement OAM logic

--- rsnes/src/oam.rs
+++ rsnes/src/oam.rs
@@ -1,19 +1,53 @@
+#[derive(Debug, Clone, Copy)]
+pub struct Object {
+    pub x: i16,
+    pub y: u8,
+    pub tile_nr: u8,
+    pub attrs: u8,
+    pub is_large: bool,
+}
+
+impl Object {
+    pub const fn new() -> Object {
+        Self {
+            x: 0,
+            y: 0,
+            tile_nr: 0,
+            attrs: 0,
+            is_large: false,
+        }
+    }
+
+    pub fn write_low_low(&mut self, val1: u8, val2: u8) {
+        self.x = (((self.x as u16) & 0x100) | u16::from(val1)) as i16;
+        self.y = val2;
+    }
+
+    pub fn write_low_high(&mut self, val1: u8, val2: u8) {
+        self.tile_nr = val1;
+        self.attrs = val2;
+    }
+
+    pub fn write_high(&mut self, val: u8) {
+        self.x = ((self.x as u16 & 0xff) | (u16::from(val & 1) << 8)) as i16;
+        self.is_large = val & 2 > 0;
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct Oam {
-    low: [u8; 512],
-    high: [u8; 32],
+    pub(crate) objs: [Object; 128],
     // 10-bit value
-    addr: u16,
-    addr_inc: u16,
+    pub(crate) addr: u16,
+    pub(crate) addr_inc: u16,
     stashed_write: u8,
-    priority: bool,
+    pub(crate) priority: bool,
 }
 
 impl Oam {
     pub const fn new() -> Self {
         Self {
-            low: [0; 512],
-            high: [0; 32],
+            objs: [Object::new(); 128],
             addr: 0,
             addr_inc: 0,
             stashed_write: 0,
@@ -34,33 +68,30 @@ impl Oam {
     }
 
     pub fn set_addr_high(&mut self, value: u8) {
-        self.addr = (self.addr & 0x1ff) | (u16::from(value & 1) << 9);
+        self.addr = (self.addr & 0x1fe) | (u16::from(value & 1) << 9);
         self.addr_inc = self.addr;
         self.priority = value & 0x80 > 0;
     }
 
-    pub fn read(&mut self) -> u8 {
-        let value = if self.addr > 0x1ff {
-            self.high[usize::from(self.addr_inc & 0x1f)]
-        } else {
-            self.low[usize::from(self.addr_inc & 0x1ff)]
-        };
-        self.addr_inc = self.addr_inc.wrapping_add(1);
-        value
-    }
-
     pub fn write(&mut self, value: u8) {
-        if self.addr > 0x1ff {
-            self.high[usize::from(self.addr_inc & 0x1f)] = value
-        } else {
-            if self.addr_inc & 1 == 0 {
-                self.stashed_write = value
-            } else {
-                self.low[usize::from(self.addr_inc & 0x1fe)] = self.stashed_write;
-                self.low[usize::from(self.addr_inc & 0x1ff)] = value;
-            }
+        let addr = self.addr_inc;
+        if addr & 1 == 0 {
+            self.stashed_write = value
         }
         self.addr_inc = self.addr_inc.wrapping_add(1);
+        if addr > 0x1ff {
+            let i = usize::from((addr & 31) << 2);
+            self.objs[i].write_high(value & 3);
+            self.objs[i | 1].write_high((value >> 2) & 3);
+            self.objs[i | 2].write_high((value >> 4) & 3);
+            self.objs[i | 3].write_high(value >> 6);
+        } else if addr & 1 == 1 {
+            [Object::write_low_low, Object::write_low_high][usize::from((addr >> 1) & 1)](
+                &mut self.objs[usize::from(addr >> 2)],
+                self.stashed_write,
+                value,
+            );
+        }
     }
 }
 
